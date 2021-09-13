92625a3657986f35307fb1ff51ed12d057576b17
Gabriel Coutini <gabriel.coutini@globalhitss.com.br>
2021-09-13 01:13:47 -0300

    schema fields are no longer required

--- resources/models/Address.js
+++ resources/models/Address.js
@@ -2,32 +2,25 @@ const mongoose = require('mongoose');
 const AddressSchema = new mongoose.Schema({
   cep: {
     type: String,
-    required: true,
   },
   logradouro: {
     type: String,
-    required: true
   },
   complemento: {
     type: String,
-    required: true
   },
   bairro: {
     type: String,
-    required: true
   },
   localidade: {
     type: String,
-    required: true
   },
   uf: {
     type: String,
-    required: true
   },
   ddd: {
     type: String,
-    required: true
   }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('address', AddressSchema);
