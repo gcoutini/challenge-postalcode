2e8ed6b41cf33d02a8b97fe80c2f3315252f30ed
Gabriel Coutini <gabriel.coutini@globalhitss.com.br>
2021-09-13 01:15:13 -0300

    adds database operations

--- resources/controllers/address.js
+++ resources/controllers/address.js
@@ -1,11 +1,35 @@
 const axios = require('axios');
 
-const address = async (req, res) => {
+const Address = require('../models/Address');
+
+const handleAddress = async (req, res) => {
   const zipcode = req.query.zipcode;
-  console.log('OLHA O ZIPCODE AI', zipcode);
   const { data } = await axios.get(`https://viacep.com.br/ws/${zipcode}/json/`);
+  if (data.erro) {
+    console.log('CEP não encontrado!');
+    return res.status(404).json({ message: "CEP não encontrado!" });
+  }
   const { ibge, gia, siafi, ...parsedAddress } = data;
-  res.send(parsedAddress);
+  const { cep } = parsedAddress;
+
+  const cepExists = await Address.findOne({ cep }); 
+
+  if (cepExists) {
+    const oneDay = 86400000;
+    const { updatedAt } = cepExists;
+    const isDocOutdated = ((new Date() - updatedAt) / oneDay) > 30 ? true : false;
+    if (isDocOutdated) {
+      await Address.updateOne({ cep }, parsedAddress);
+      console.log('O CEP foi atualizado!');
+      return res.status(200).json(parsedAddress);
+    }
+    console.log('CEP cadastrado há menos de 30 dias!');
+    return res.status(400).json(parsedAddress);
+  }
+  let address = new Address(parsedAddress);
+  await address.save();
+  console.log('CEP cadastrado com sucesso!');
+  res.status(201).json(parsedAddress);
 }
 
-module.exports = address;
+module.exports = handleAddress;
